4ea0526ea446418b6aba008e2dd699cdf12810b0
lee <94dlrkdtks@naver.com>
2022-05-13 01:01:35 +0900

    [이강산]

--- src/main/java/com/ashspell/movie/user/UserRestController.java
+++ src/main/java/com/ashspell/movie/user/UserRestController.java
@@ -18,7 +18,7 @@ import com.ashspell.movie.user.bo.UserBO;
 import com.ashspell.movie.user.model.User;
 
 @RestController
-@RequestMapping("/movie")
+@RequestMapping("/movie	")
 public class UserRestController {
 	
 	@Autowired
